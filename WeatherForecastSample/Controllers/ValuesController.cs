5cff920aee60a4e4d359bc950a0bd988f989a24b
nikchoudhary <nikhilexamination@gmail.com>
2020-09-11 10:50:45 +0530

    Second Commit

---
+++ WeatherForecastSample/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecastSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getVal")]
+
+        public string getdetails()
+        {
+            return "My name is Nikhil";
+        }
+    }
+}
